fed865b62dab58930b605b877266564d8c8de8f7
Billy <billy@meldcxs-MBP.localdomain>
2019-08-29 13:17:38 +1000

    typescript config

---
+++ src/components/GlobalStyle/index.tsx
@@ -0,0 +1,15 @@
+import { createGlobalStyle } from "styled-components";
+
+export default createGlobalStyle`
+  body {
+        margin: 0;
+        padding: 0;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        min-height: 100vh;
+        font-family: "Varela Round", sans-serif;  
+        user-select: none;
+    
+      }
+`;
